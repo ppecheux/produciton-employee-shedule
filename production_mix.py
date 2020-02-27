af57f3ada58e1ff20f9068c90846f294b44279ea
ppecheux <pierrelouis.pecheux@gmail.com>
2020-02-27 17:08:41 +0100

    graph with legend

--- production_mix.py
+++ production_mix.py
@@ -1,6 +1,10 @@
-from numpy import mean, array_split, random
-from math import log2
 
+#%%
+from numpy import mean, array_split, random, cumsum
+import numpy as np
+from math import log2
+from matplotlib import pyplot as plt
+#%%
 def mix_distribute_uniformly(arr : iter):
     """shuffles the array based on the mean of the value of the item
     
@@ -39,7 +43,7 @@ def merge_mix(arr: list) -> list:
         arr = [item for split in arr for item in split]        
         level /= 2
     return arr
-
+#%%
 arr = [12, 11, 13, 5, 6, 7]
 arr = random.randint(0,high=1000, size=20)
 arr_verif = arr.copy()
@@ -47,8 +51,16 @@ n = len(arr)
 print ("Given array is")
 print(arr)
 
-  
+print ("\n\nmixed array is")
+mix_arr = merge_mix(arr)
+print(mix_arr)
 
-print ("\n\nSorted array is")
-print(merge_mix(arr))
-print(all(arr == sorted(arr_verif)))
+plt.plot(arr, label='random distribution of trucks')
+plt.plot(mix_arr, label='mixed distribution of trucks')
+plt.plot( [mean(arr)]*len(arr), label='mean time for a truck')
+plt.plot(cumsum(arr)/(np.arange(len(arr)) + np.ones(len(arr))), label='cumulated mean time for the random distribution')
+plt.plot(cumsum(mix_arr)/(np.arange(len(arr)) + np.ones(len(arr))), label='cumulated mean time for the mixed distribution')
+plt.legend()
+plt.xlabel("sequence of trucks")
+plt.ylabel("time to produce a truck")
+plt.show()
