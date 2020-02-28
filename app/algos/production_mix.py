812f282044d39061942ce4724347e8d5f9287e51
ppecheux <pierrelouis.pecheux@gmail.com>
2020-02-28 16:19:00 +0100

    graph production mix

--- app/algos/production_mix.py
+++ app/algos/production_mix.py
@@ -1,10 +1,9 @@
 
-#%%
 from numpy import mean, array_split, random, cumsum
 import numpy as np
 from math import log2
 from matplotlib import pyplot as plt
-#%%
+
 def mix_distribute_uniformly(arr : iter):
     """shuffles the array based on the mean of the value of the item
     
@@ -43,24 +42,26 @@ def merge_mix(arr: list) -> list:
         arr = [item for split in arr for item in split]        
         level /= 2
     return arr
-#%%
-arr = [12, 11, 13, 5, 6, 7]
-arr = random.randint(0,high=1000, size=20)
-arr_verif = arr.copy()
-n = len(arr) 
-print ("Given array is")
-print(arr)
 
-print ("\n\nmixed array is")
-mix_arr = merge_mix(arr)
-print(mix_arr)
+if __name__ == "__main__":
 
-plt.plot(arr, label='random distribution of trucks')
-plt.plot(mix_arr, label='mixed distribution of trucks')
-plt.plot( [mean(arr)]*len(arr), label='mean time for a truck')
-plt.plot(cumsum(arr)/(np.arange(len(arr)) + np.ones(len(arr))), label='cumulated mean time for the random distribution')
-plt.plot(cumsum(mix_arr)/(np.arange(len(arr)) + np.ones(len(arr))), label='cumulated mean time for the mixed distribution')
-plt.legend()
-plt.xlabel("sequence of trucks")
-plt.ylabel("time to produce a truck")
-plt.show()
+    arr = [12, 11, 13, 5, 6, 7]
+    arr = random.randint(0,high=1000, size=20)
+    arr_verif = arr.copy()
+    n = len(arr) 
+    print ("Given array is")
+    print(arr)
+
+    print ("\n\nmixed array is")
+    mix_arr = merge_mix(arr)
+    print(mix_arr)
+
+    plt.plot(arr, label='random distribution of trucks')
+    plt.plot(mix_arr, label='mixed distribution of trucks')
+    plt.plot( [mean(arr)]*len(arr), label='mean time for a truck')
+    plt.plot(cumsum(arr)/(np.arange(len(arr)) + np.ones(len(arr))), label='cumulated mean time for the random distribution')
+    plt.plot(cumsum(mix_arr)/(np.arange(len(arr)) + np.ones(len(arr))), label='cumulated mean time for the mixed distribution')
+    plt.legend()
+    plt.xlabel("sequence of trucks")
+    plt.ylabel("time to produce a truck")
+    plt.show()
--- app/views/mix_page.py
+++ app/views/mix_page.py
@@ -5,7 +5,8 @@ from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import dash_table
 from app import app
-
+from algos.production_mix import merge_mix
+import numpy as np
 table_colums = {"name": "text", "time": "numeric", "quantity": "numeric"}
 
 @app.callback(
@@ -19,8 +20,65 @@ def add_row(n_clicks, rows, columns):
     rows.append({c['id']: '' for c in columns})
     return rows
 
+@app.callback(
+    Output('table_suggested_order', 'data'),
+    [Input('table_initial_quantity_time', 'data')]
+)
+def data_table_suggested_order(init_data):
+    if not init_data:
+        PreventUpdate
+
+    #create a list of time needed for each product
+    times = []
+    for row in init_data:
+        if '' not in (row['name'], row['time'], row['quantity']):
+            times.extend([float(row.get('time',0))]*int(row.get('quantity',0)))
+
+    mixed_times = merge_mix(times)
+    #create data for the suggested order table
+    suggested_data = []
+    cumulated_time = 0
+    for time in mixed_times:
+        cumulated_time += time
+        for row in init_data:
+            if '' not in (row['name'], row['time'], row['quantity']) and int(row.get('quantity',0)) > 0 and float(row.get('time',0)) == time:
+                suggested_data.append({'name': row.get('name',None), 'time': time, 'cumulated_time': cumulated_time})
+                row['quantity'] -= 1
+
+    return suggested_data
+                
+@app.callback(
+    Output('graph_suggested_order', 'figure'),
+    [Input('table_suggested_order', 'data')]
+)
+def figure_graph_suggested_order(table_data):
+    if not table_data:
+        raise PreventUpdate
+
+    table_data_names = [row['name'] for row in table_data]
+    table_data_times = [float(row['time']) for row in table_data]
+    data = [
+        {
+            'x': [indice for indice, v in enumerate(table_data_names) if v == name],
+            'y': [table_data_times[table_data_names.index(name)]]*table_data_names.count(name),
+            'type': 'bar',
+            'name': name
+        } for name in set(table_data_names)] + [
+        {'x': list(range(len(table_data_names))), 'y': [np.mean(table_data_times)]*len(table_data_names), 'name': 'average production time'}
+    ]
+
+    figure = {
+        'data': data,
+        'layout': {
+            'title': 'order of production visualization',
+            'xaxis': {'title': 'rank on the production line'},
+            'yaxis': {'title': 'production time'}
+        }
+    }
+    return figure
+
 layout = dbc.Container([
-    html.H1('List of product needed to be produce'),
+    html.H1('List of product needed to be produced'),
     dash_table.DataTable(
         id='table_initial_quantity_time',
         columns=[{'id': name, 'name': name, 'type': type} for name, type in table_colums.items()],
@@ -37,6 +95,9 @@ layout = dbc.Container([
             {'id': 'time', 'name': 'time'},
             {'id': 'cumulated_time', 'name': 'cumulated_time'}
         ]
+    ),
+    dcc.Graph(
+        id='graph_suggested_order'
     )
 ])
 
