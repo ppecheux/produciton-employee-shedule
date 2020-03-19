e0b2725bf43cb947b49b757594e619f270bfaaef
leaG30 <lea.garbin@gmail.com>
2020-03-19 10:56:56 +0100

    'Weighted_avg'

--- algos/stations.py
+++ algos/stations.py
@@ -1,4 +1,4 @@
 "In this file, we want to assign stations to activity blocks"
 
 def assign_stations(activities: list, nb_stations: int) -> dict:
-    return activities
+	return activities
--- tests/test_stations.py
+++ tests/test_stations.py
@@ -15,11 +15,51 @@ class TestAssignStations(unittest.TestCase):
             "duration":  [pd.Timedelta(minutes=1), pd.Timedelta(minutes=1), pd.Timedelta(minutes=1), pd.Timedelta(minutes=1)],
             "station": [np.nan]*4
         })
+        print(df)
         records = df.to_json(orient='records')
+        print(records)
         records_with_stations = assign_stations(activities=records, nb_stations=1)
         df_with_stations = pd.read_json(records_with_stations, orient='records')
 
         self.assertFalse(df_with_stations.station.isnull().values.any())
+
+    def weighted_average(self):
+
+        #Creating a dataframe for the test
+
+        df = pd.DataFrame({
+            "cabineA":[12, 15, 25,24],
+            "cabineB":[26,27,19,12],
+            "cabineC":[14,28,22,16]},
+            index = ["act1","act2", "act3", "act4"])
+
+        Nb_cabs = [20, 14, 8]  #Number of cabs in a list
+
+        tab = df.to_numpy()    #Dataframe to numpy to be able to calculate the weighted average
+
+        nb_act =tab.shape[0]   #Number of activities
+
+        Weig_avg = []          #Creating an empty list where we will put the weighted avg for each activity
+
+        avg = 0                #temp variables
+        Nb_cabs_tot = 0
+
+
+        for i in range (len(Nb_cabs)-1):
+            Nb_cabs_tot += Nb_cabs[i]       #Number total of cabs
+
+        for i in range (nb_act-1):          #Determination of the weighted avg of each activity
+            for j in range (2):
+                avg += tab[i][j] * Nb_cabs[j]
+            avg = avg/Nb_cabs_tot
+            Weig_avg.append(avg)
+
+
+        print(Weig_avg)
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
