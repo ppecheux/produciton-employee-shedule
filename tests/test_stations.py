32d622d3d42b610d6f3aff1aa8a9fa67abbd7626
ppecheux <pierrelouis.pecheux@gmail.com>
2020-03-19 10:30:02 +0100

    prepare some new tests

--- tests/test_stations.py
+++ tests/test_stations.py
@@ -21,5 +21,38 @@ class TestAssignStations(unittest.TestCase):
 
         self.assertFalse(df_with_stations.station.isnull().values.any())
 
+    def test_limited_nb_of_stations_used(self):
+        nb_stations = 2
+        df = pd.DataFrame({
+            "cabine": ["cabineA", "cabineA", "cabineB", "cabineB"],
+            "activity": ["activity1", "activity2", "activity1", "activity2"],
+            "duration":  [pd.Timedelta(minutes=1), pd.Timedelta(minutes=1), pd.Timedelta(minutes=1), pd.Timedelta(minutes=1)],
+            "station": ['sation1', 'station3', 'station2', 'station3']
+        })
+        records = df.to_json(orient='records')
+        records_with_stations = assign_stations(activities=records, nb_stations=nb_stations)
+        df_with_stations = pd.read_json(records_with_stations, orient='records')
+
+        self.assertTrue(len(df_with_stations.station.unique())<=nb_stations)
+
+    def test_stations_are_not_overlapping(self):
+        df = pd.DataFrame({
+            "cabine": ["cabineA", "cabineA", "cabineB", "cabineB"],
+            "activity": ["activity1", "activity2", "activity1", "activity2"],
+            "duration":  [pd.Timedelta(minutes=1), pd.Timedelta(minutes=1), pd.Timedelta(minutes=1), pd.Timedelta(minutes=1)],
+            "station": ['sation1', 'station3', 'station2', 'station3']
+        })
+
+        records = df.to_json(orient='records')
+        records_with_stations = assign_stations(activities=records, nb_stations=1)
+        df = pd.read_json(records_with_stations, orient='records')
+
+        for station in df.station.unique():
+            df_station = df[df.station == station]
+            self.assertEqual(df_station.station.count(), 1 + df_station.index.max() - df_station.index.min())
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
