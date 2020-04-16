347e091503a90e561ed5b25895ea3ed2afea068d
Teyshaun Beynon <teyshaun@gmail.com>
2020-04-16 16:53:39 +0200

    test

--- views/mix_page.py
+++ views/mix_page.py
@@ -113,7 +113,7 @@ def figure_graph_suggested_order(table_data, input_shift_duration_hour, input_op
 layout = dbc.Container([
     html.H3('Change takt time by tweaking these parameters: '),
     takt_time_input,
-    html.H1('List of product needed to be produced'),
+    html.H1('List of product needed to be produced',style={'color':'red'}),
     dcc.Upload(
         id='upload_mix_data',
         children=html.Div(
