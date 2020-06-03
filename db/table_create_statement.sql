1062177ca0d70623fd6305967af6574d2eb2846d
ppecheux <pierrelouis.pecheux@gmail.com>
2020-06-03 13:34:44 +0200

    max_sequence_rank added to db

--- db/table_create_statement.sql
+++ db/table_create_statement.sql
@@ -10,5 +10,7 @@ CREATE TABLE IF NOT EXISTS activity
     activity_block_name varchar(80),
     activity_block_duration INTEGER,
     station_nb INTEGER,
+    max_sequence_rank varchar(20),
+    min_sequence_rank varchar(20),
     PRIMARY KEY(product, activity_block_name)
     );
