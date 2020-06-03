5450f94939b387dec2b066161b300e96a1db46b8
ppecheux <pierrelouis.pecheux@gmail.com>
2020-06-03 13:43:49 +0200

    remove unsused code db

--- db/table_create_statement.sql
+++ db/table_create_statement.sql
@@ -1,10 +1,3 @@
-CREATE TABLE IF NOT EXISTS user 
-    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username varchar(20) UNIQUE NOT NULL,
-    email varchar(50) UNIQUE NOT NULL,
-    password varchar(80) NOT NULL,
-    admin BOOLEAN NOT NULL DEFAULT FALSE);
-
 CREATE TABLE IF NOT EXISTS activity
     (product varchar(80),
     activity_block_name varchar(80),
