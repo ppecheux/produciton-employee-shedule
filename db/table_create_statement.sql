66c32ccf3ec6608cc555f04579762a9137f94382
ppecheux <pierrelouis.pecheux@gmail.com>
2020-06-03 13:30:48 +0200

    change db type

--- db/table_create_statement.sql
+++ db/table_create_statement.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS user
 CREATE TABLE IF NOT EXISTS activity
     (product varchar(80),
     activity_block_name varchar(80),
-    activity_block_duration datetime,
+    activity_block_duration INTEGER,
     station_nb INTEGER,
     PRIMARY KEY(product, activity_block_name)
     );
