1bf1724020489ed60e7ccb607af30836ee0ecfd3
MartinBoyerpro <57049702+MartinBoyerpro@users.noreply.github.com>
2020-06-17 16:15:47 +0200

    Update tutorials.py

--- views/tutorials.py
+++ views/tutorials.py
@@ -1,108 +1,124 @@
 
 import dash_html_components as html
 
-layout = html.Div(
-    className='pageContent2', children=[
-        html.H1("Tutorial Page"),
-        html.Div(className='tutorialTitle', children=[
-            "ESTAÇÕES"]),
-        html.Div(className='tutorialText', children=[
-            "Da página inicial vamos para a parte [então] de estações. "]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Demonstrando um poucoa interface do programa, temos [então] o balanço de estações, onde podemos demonstrar aquantidade de horas que um trabalhador pode estar em turno de trabalho, além da eficiência do trabalhador para mais ou para menos."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Também podemos alterar a quantidade de estações da linha de produção."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Outro tópico é a lista de atividade de produção, onde podemos fazer por upload arrastando simplesmente os arquivos como ser na imagem e ainda de modo manual adicionando linha por linha de trabalho. "]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Além disso podemos entrar com os produtos necessários e as quantidades de produção, neste caso os produtos seriam os modelos de caminhão."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Sendo assim temos a sugestão de estações para os blocos de atividades da linha de produção."]),
-        html.Br(),
-        html.A("Video ESTAÇÕES",
-               href='https://www.youtube.com/watch?v=TT8KuCXRKJ8', target="_blank"),
+layout =html.Div(id='pageContent2',children=[
+ 	 html.H1("Tutorial Page"),
+ 	 html.Div(id='tutorialTitle',children=[
+ 	 		"ESTAÇÕES"]),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"Para fazer o balanceamento das estações na linha de produção, siga os passos abaixo: "]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"1 -   Definir o Takt Time:"]),
+ 	 		html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"Inserir a duração do turno de trabalho, em horas, e também a eficiência do operador em termos percentuais. Essas duas entradas irão computar o takt time da produção."]),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"2 -   Definir o número de estações na linha de produção."]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"3 -   Entrada das listas de atividades que deverão ser realizadas."]),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"Para isso existem duas vias :"]),
+	html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"A -   Upload de um arquivo .csv ou .xls"]),
+	html.Br(),
+ 	  html.Div(id='tutorialTextcorps',children=[
+ 	 		"O arquivo .csv ou .xls poderá ser inserido arrastando e soltando o arquivo na área de upload ou clicando sobre esta para a seleção do arquivo no computador. É importante ressaltar que a ordem dos dados nas colunas deste arquivo devem ser as especificadas. Após carregamento da planilha os dados deverão aparecer na tabela logo abaixo à área de upload. Nesta tabela ainda é possível editar os dados carregados anteriormente ou ainda acrescentar novas atividades. (Para isto seguir instruções do item b)."]),
+html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"B -   Entrada manual dos dados na tabela :"]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		" Logo abaixo da área de upload existe uma tabela onde podem ser adicionados manualmente os dados. Para isto basta clicar em “adicionar linha” e alimentar a tabela.  Para editar dados basta selecionar a célula que deseja editar e adicionar a nova informação. Para criar novas linhas na tabela, clicar sobre “ criar linha” e para remover uma linha é preciso apenas clicar sobre o “X” no lado esquerdo da linha da atividade."]),
+	html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"4 -   Adicionar os modelos que serão produzidos e quantidades."]), 
+html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"5 -   Feito isto o software fará a sugestão das atividades que deverão ser alocadas em cada estação."]),
+html.Br(),
+	html.A("Tutorial ESTAÇÕES 1", href='https://www.youtube.com/watch?v=TT8KuCXRKJ8&feature=youtu.be', target="_blank"),
+	html.Br(),
+	html.A("Tutorial ESTAÇÕES 2", href='https://www.youtube.com/watch?v=jbLRArqjYdI&feature=youtu.be', target="_blank"),
+##########################################################
+	 html.Div(id='tutorialTitle',children=[
+ 	 		"OPERADORES"]),
+	 html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"Para fazer a distribuição de tarefas aos operadores, siga os passos abaixo:"]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"1 -   Definir o Takt Time:"]),
+ 	 		html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"Inserir a duração do turno de trabalho, em horas, e também a eficiência do operador em termos percentuais. Essas duas entradas irão computar o takt time da produção."]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"2 -   Definir o número de estações na linha de produção."]),
+ 	 		html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"3 -   Entrada das listas de atividades que deverão ser realizadas."]),
+	html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"Para isso existem duas vias:"]),
+ 	 		html.Br(),
+ 	 		 html.Div(id='tutorialTextcorps',children=[
+ 	 		"A -   Upload de um arquivo .csv ou .xls"]),
+	html.Br(),
+ 	  html.Div(id='tutorialTextcorps',children=[
+ 	 		"O arquivo .csv ou .xls poderá ser inserido arrastando e soltando o arquivo na área de upload ou clicando sobre esta para a seleção do arquivo no computador. É importante ressaltar que a ordem dos dados nas colunas deste arquivo devem ser as especificadas. Após carregamento da planilha os dados deverão aparecer na tabela logo abaixo à área de upload. Nesta tabela ainda é possível editar os dados carregados anteriormente ou ainda acrescentar novas atividades. (Para isto seguir instruções do item b)."]),
+html.Br(),
+		 html.Div(id='tutorialTextcorps',children=[
+ 	 		"B -   Entrada manual dos dados na tabela :"]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		" Logo abaixo da área de upload existe uma tabela onde podem ser adicionados manualmente os dados. Para isto basta clicar em “adicionar linha” e alimentar a tabela.  Para editar dados basta selecionar a célula que deseja editar e adicionar a nova informação. Para criar novas linhas na tabela, clicar sobre “ criar linha” e para remover uma linha é preciso apenas clicar sobre o “X” no lado esquerdo da linha da atividade."]),
+	html.Br(),
 
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"3 -   Adicionar os modelos que serão produzidos e quantidades."]),
+html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"Feito isto o software fará a sugestão das atividades que deverão ser alocadas para cada operador."]),
+ 	 		html.Br(),
+ 	 		html.A("Tutorial OPERADORES 1", href='https://youtu.be/eodK72IpzN0', target="_blank"),
+	html.Br(),
+			html.A("Tutorial OPERADORES 2", href='https://www.youtube.com/watch?v=SQkwEOELs4E&feature=youtu.be', target="_blank"),
+	html.Br(),
+	##########################################
+	html.Div(id='tutorialTitle',children=[
+ 	 		"MIX DE PRODUÇÃO"]),
+	
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"Para fazer o mix de produção, siga os passos abaixo:"]),
+html.Br(),
+html.Div(id='tutorialText',children=[
+ 	 		"1 -   Definir o Takt Time:"]),
+ 	 		html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"Inserir a duração do turno de trabalho, em horas, e também a eficiência do operador em termos percentuais. Essas duas entradas irão computar o takt time da produção."]),
+ 	 html.Div(id='tutorialText',children=[
+ 	 	"2 -   Entrada da lista modelos que deverão ser montados ."]),
+ 	 		html.Br(),
+ 	 		html.Div(id='tutorialTextcorps',children=[
+ 	 		"Para isso existem duas vias:"]),
+ 	 		html.Br(),
+ 	 		html.Div(id='tutorialTextcorps',children=[
+ 	 		"A -   Upload de um arquivo .csv ou .xls"]),
+	html.Br(),
+ 	 html.Div(id='tutorialTextcorps',children=[
+ 	 		"O arquivo .csv ou .xls poderá ser inserido arrastando e soltando o arquivo na área de upload ou clicando sobre esta para a seleção do arquivo no computador. É importante ressaltar que a ordem dos dados nas colunas deste arquivo devem ser as especificadas. Após carregamento da planilha os dados deverão aparecer na tabela logo abaixo à área de upload. Nesta tabela ainda é possível editar os dados carregados anteriormente ou ainda acrescentar novos modelos. (Para isto seguir instruções do item b)."]),
+html.Br(),
+		html.Div(id='tutorialTextcorps',children=[
+ 	 		"B -   Entrada manual dos dados na tabela :"]),
+ 	 html.Br(),
+ 	 html.Div(id='tutorialText',children=[
+ 	 		"3 -   Feito isto o software fará a sugestão das atividades que deverão ser alocadas para cada operador."]),
+html.Br(),
+ 	 		html.A("Video MIX DE PRODUÇÃO", href='https://www.youtube.com/watch?v=XBeyy7_5igc&feature=youtu.be', target="_blank"),
+ 	 
+ 	 ]) 
 
-        html.Div(className='tutorialText', children=[
-            "Após definir a quantidade de horas de trabalho, a eficiência do operador e o número de estações, podemos partir [então] para a lista de atividades. Neste exemplo iremos utilizar uma planilha pré-criada podendo ela ser do Excel ou do Google planilhas onde teremos então a definição de produtos, blocos de atividades, duração de atividades e a estação a qual a mesma está fixada."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            " É importante ressaltar que o cabeçalho dessa planilha está exatamente igual como na interface: product, activity block name, activity block duration e fixed station number."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Basta simplesmente realizar o download desta planilha em formato .csv (dados separados por vírgula) e [então], retornando para a interface de trabalho, basta arrastar documento baixado para a interface, logo, temos todos os dados de nossa planilha na interface de trabalho."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Uma questão bastante interessante é que se pode realizar  alterações na planilha de modo livre, mudando quaisquer dados quanto for necessário."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Descendo um pouco mais na interface, temos a quantidade de produtos (neste caso, caminhões) a serem produzidos, sendo assim escolhidos de acordo com a necessidade para as ordens de produção."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Descendo um pouco mais, temos [então] a sugestão de estações para o bloco de atividades."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "O último tópico da interface demonstra de maneira visual como foi definida a sugestão do bloco de atividades."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Porém, caso seja necessário alterações, basta simplesmente voltar ao topo da interface e novamente alterar como necessário, seja esta alteração nas horas de trabalho, no número de estações ou na eficiência do operador, tendo assim novos dados gráficos."]),
-        html.Br(),
-
-
-        html.Div(className='tutorialTitle', children=[
-            "OPERADORES"]),
-        html.Div(className='tutorialText', children=[
-            "Atentando-se agora a distribuição de tarefas, iremos para interface de operadores. Neste caso temos [então] o tempo de duração de horas do operador além da eficiência de cada operador. Abaixo temos a janela para o arraste de upload da lista de atividades de produção ou ainda adição manual de cada atividade."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Logo abaixo temos a quantidade de caminhões a serem produzidos, como no exemplo anterior, além da sugestão de atividades para cada operador."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Após a seleção das horas de trabalho, além da eficiência de operação, iremos [então] para uma lista pré-construída no Google planilhas ou no Excel com a definição do produto, do bloco de atividade, da duração da atividade, além do número da estação de realização da atividade."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "É importante ressaltar que cada uma das colunas tem como título o nome adequado como descrito na interface."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Para realizar este download basta fazê-lo com a extensão .csv (dados separados por vírgula) e após o download basta apenas arrastar o mesmo até a janela da lista de atividades, tendo assim todas as atividades elencadas como mostrado no vídeo."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "É importante ressaltar que é possível realizar mudanças como sejam necessárias (a qualquer momento)."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Descendo um pouco mais pela interface, conseguimos ainda, quantificar o número de cada modelo de caminhão que será produzido, [então] obtemos a sugestão de atividades para cada operador."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "No final da página verificamos [então] de modo visual como se resulta a distribuição de atividades para cada operador, novamente, mudanças podem ser realizadas caso sejam necessárias, retornando para a tabela que se realizou o upload, mudando as horas de duração e também a eficiência de operação, obtendo assim novos resultados."]),
-        html.Br(),
-
-
-        html.Div(className='tutorialTitle', children=[
-            "MIX DE PRODUÇÃO"]),
-        html.Div(className='tutorialText', children=[
-            "Atentando-se a otimização na linha de produção, pensando assim construir a maior quantidade de caminhões, em tempo hábil possível vamos então para a interface do mix de produção."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Neste caso temos então a quantidade de horas trabalhadas além da eficiência do operador. A janela abaixo lista os produtos a serem adicionados de forma a se fazer um upload ou de modo manual, logo tendo então uma sugestão da ordem de produção da linha de caminhões."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "A partir de uma planilha pré selecionada, seja ela no Google planilhas ou no Excel, definindo: o nome ou no caso o modelo do caminhão, o tempo de produção e a quantidade a ser produzida (observa-se que os títulos devem estar idênticos como na interface, fazemos o download do arquivo em extensão csv - dados separados por vírgula)."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Então retornando para a interface, podemos arrastar o arquivo para a janela indicada. É importante ressaltar que mudanças podem ser realizadas nos dados obtidos."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "[Então] temos a ordem de sugestão da linha de produção, o gráfico abaixo demonstra a utilização desta linha de produção a partir de análise de tempo para cada modelo de caminhão fabricado."]),
-        html.Br(),
-        html.Div(className='tutorialText', children=[
-            "Ainda é possível fazer alterações apenas com a mudança nos lugares indicados ou até mesmo na planilha criada, obtendo assim diferentes resultados de acordo com a necessidade da planta."]),
-        html.Br(),
-
-
-    ]
-)
+		
